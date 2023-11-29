ecc1ad92af8a5d4f95fcbb5b57fad80bf6759f95
SheikhAmin785 <shakil785ks@gmail.com>
2023-11-29 11:24:48 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace PetyaandStrings
+{
+    class Program
+    {
+        static void Main()
+        {
+
+                string str1 = Console.ReadLine();
+
+                string str2 = Console.ReadLine();
+
+                string  a= str1.ToLower();
+
+                string b = str2.ToLower();
+
+                if (a ==b)
+                {
+                    Console.WriteLine("0");
+                }
+                else
+                {
+                    for (int i = 0; i < a.Length; i++)
+                    {
+                        if (a[i] < b[i])
+                        {
+                            Console.WriteLine("-1");
+                            break;
+                        }
+                        if (a[i] >b[i])
+                        {
+                            Console.WriteLine("1");
+                            break;
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+    
+
